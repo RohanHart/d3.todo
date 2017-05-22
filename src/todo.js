9495f54618a3d6960441809be4d2fba5a04e382a
rohan <rohan.hart@fronde.com>
2017-05-22 12:50:37 +1200

    use <li> as template

--- src/todo.js
+++ src/todo.js
@@ -10,10 +10,19 @@
   // rename the base d3 abstraction!
   d3.selection.prototype.with = d3.selection.prototype.call
 
+  // simple HTML based template support with **NO** magic HTML extensions!!!
+  d3.selection.prototype.asTemplate = function asTemplate () {
+    var node = this.remove().node()
+
+    return function () {
+      return node.cloneNode(true)
+    }
+  }
+
   function todoWidget (config) {
     var items = []
     var uid = 0
-    var todoLines
+    var todoLines, lineTemplate
 
     (function () {
       var container = d3.select(config.container)
@@ -22,6 +31,7 @@
         inputBox(container)))
 
       todoLines = todoList(container)
+      lineTemplate = todoLines.asTemplate()
 
       return this
 
@@ -45,7 +55,7 @@
 
       function todoList (container) {
         return container.select('#list')
-          .selectAll('*')  // will replace all the existing nodes
+          .selectAll('#list > *')  // will replace all the existing nodes
       }
 
       function addItem (itemText) {
@@ -77,19 +87,16 @@
       return this
 
       function todoLine (container) {
-        return container.append('li')
-          .attr('class', 'item')
+        return container.append(lineTemplate)
           .with(doneToggle)
           .with(label)
           .with(removeButton)
 
         function doneToggle (line) {
-          return line.append('input')
-            .attr('type', 'checkbox')
-            .attr('class', 'state-button')
-            .on('click', setItemState)
+          return line.select('[data-done]')
+            .on('click', toggleDone)
 
-          function setItemState (item) {
+          function toggleDone (item) {
             item.isDone = !item.isDone
             renderList()
 
@@ -98,17 +105,14 @@
         }
 
         function label (line) {
-          return line.append('span')
-            .attr('class', 'label')
+          return line.select('[data-label]')
             .text(function (d) {
               return d.label
             })
         }
 
         function removeButton (line) {
-          return line.append('span')
-            .attr('class', 'delete-button')
-            .html('&#10060')
+          return line.select('[data-delete]')
             .on('click', removeItem)
 
           function removeItem (item) {
